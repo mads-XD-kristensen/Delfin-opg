18d62656e1cb29634bd7fecc68f486e219612630
AlbertSylv <basketasl@hotmail.com>
2019-11-26 15:45:16 +0100

    albert commit

--- DelleFin/src/dellefin/delleFin.java
+++ DelleFin/src/dellefin/delleFin.java
@@ -5,8 +5,7 @@ package dellefin;
 public class delleFin {
 
     public static void main(String[] args) {
-        controller cntr = new controller();
-        cntr.start();
+        System.out.println("sut min pik");
     }
     
 }
