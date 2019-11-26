3c558b77aef25f532d2a80f745f72c7eaa0e202c
AlbertSylv <basketasl@hotmail.com>
2019-11-26 14:35:02 +0100

    albert commit2

---
+++ DelleFin/src/dellefin/medlem.java
@@ -0,0 +1,41 @@
+
+package dellefin;
+
+public class medlem {
+    String stamOpl;
+    //Ændre alder til fødselsdato, hvis der er tid og lyst
+    int alder;
+    boolean passivAktiv;
+
+    public medlem(String stamOpl, int alder, boolean passivAktiv) {
+        this.stamOpl = stamOpl;
+        this.alder = alder;
+        this.passivAktiv = passivAktiv;
+    }
+    public String getStamOpl() {
+        return stamOpl;
+    }
+
+    public void setStamOpl(String stamOpl) {
+        this.stamOpl = stamOpl;
+    }
+
+    public int getAlder() {
+        return alder;
+    }
+
+    public void setAlder(int alder) {
+        this.alder = alder;
+    }
+
+    public boolean isPassivAktiv() {
+        return passivAktiv;
+    }
+
+    public void setPassivAktiv(boolean passivAktiv) {
+        this.passivAktiv = passivAktiv;
+    }
+    
+
+    
+}
