f2406974f0283e73c3ea5903be1f8cdd11efba0a
Mads Overgaard <mco@persano.dk>
2019-11-27 14:26:16 +0100

    timezonefix

--- DelleFin/src/dellefin/DataConnector.java
+++ DelleFin/src/dellefin/DataConnector.java
@@ -4,15 +4,19 @@ package dellefin;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DataConnector {
 
     public static Connection getConnection() throws SQLException {
-        String url = "jdbc:mysql://root@localhost:3306/delfin";
+        String url = "jdbc:mysql://root@localhost:3306/delfin?serverTimezone=UTC";
         //establish connection object
         
-        Connection conn = DriverManager.getConnection(url, "root", "euy27brq");
-
+        Connection conn = null;
+       
+            conn = DriverManager.getConnection(url, "root", "ugz28shd");
+        
         return conn;
     }
 
