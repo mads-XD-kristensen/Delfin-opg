cc81184c91e2d0457de2b611ed5c1b4b01dd6b33
AlbertSylv <basketasl@hotmail.com>
2019-11-27 14:03:25 +0100

    albert commit

--- DelleFin/src/dellefin/DataConnector.java
+++ DelleFin/src/dellefin/DataConnector.java
@@ -10,6 +10,7 @@ public class DataConnector {
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:mysql://root@localhost:3306/delfin";
         //establish connection object
+        
         Connection conn = DriverManager.getConnection(url, "root", "euy27brq");
 
         return conn;
