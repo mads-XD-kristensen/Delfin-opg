38b27fe635b4d7ecd3cdbfc356e6649be848fb0b
mads-XD-kristensen <mads-XD-kristensen@users.noreply.github.com>
2019-11-27 15:15:22 +0100

    slettet motion og konkurrence medlem

--- delleFin/src/dellefin/DataConnector.java
+++ delleFin/src/dellefin/DataConnector.java
@@ -4,14 +4,23 @@ package dellefin;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DataConnector {
 
-    public static Connection getConnection() throws SQLException {
-        String url = "jdbc:mysql://root@localhost:3306/delfin";
+    public static Connection getConnection(){
+        String url = "jdbc:mysql://root@localhost:3306/delfin?serverTimezone=UTC";
         //establish connection object
-        Connection conn = DriverManager.getConnection(url, "root", "uqt42vqx");
-
+        
+        Connection conn = null;
+       
+        try {
+            conn = DriverManager.getConnection(url, "root", "uqt42vqx");
+        } catch (SQLException ex) {
+            Logger.getLogger(DataConnector.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         return conn;
     }
 
